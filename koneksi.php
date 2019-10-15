d391f2a5a11709d8fa9c0cca15c39fe98f9cc528
Dimas Pramudya Haqqi <55182072+dimasssph@users.noreply.github.com>
2019-10-15 19:20:12 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,4 @@
 <?php
 mysql_connect("localhost","root",""); //sesuaikan dengan password dan username mysql anda
-mysql_select_db("sampah_overview"); //nama database yang kita gunakan
+mysql_select_db("sampah_overview"); //nama database yang kita gunakan sampah_overview
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,35 @@
 <?php
 include 'koneksi.php';
 
-$username = $_POST['name'];
-$password = $_POST['city'];
 
-$query = mysql_query("select * from customer where username='$username' and password='$password'");
-$cek = mysql_num_rows($query);
-echo $cek;
+if(isset($_POST['login'])){
+
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+  // -- ganti users dengan nama database
+    $sql = "SELECT * FROM users WHERE username=:username";
+    $stmt = $db->prepare($sql);
+
+    // bind parameter ke query
+    $params = array(
+        ":username" => $username,
+    );
+
+    $stmt->execute($params);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // jika user terdaftar
+    if($user){
+        // verifikasi password
+        if(password_verify($password, $user["password"])){
+            // buat Session
+            session_start();
+            $_SESSION["user"] = $user;
+            // login sukses, alihkan ke halaman timeline
+            header("Location: index.html");
+        }
+    }
+}
 ?>
